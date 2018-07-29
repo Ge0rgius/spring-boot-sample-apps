614f57e465050338b462e80dcf2fa0a43c206c70
y-palevich <all4set@gmail.com>
2018-07-29 12:07:15 +0300

    First commit spring-boot samples.

---
+++ initializr-demo/src/main/java/com/example/app/initializrdemo/InitializrDemoApplication.java
@@ -0,0 +1,12 @@
+package com.example.app.initializrdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InitializrDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InitializrDemoApplication.class, args);
+	}
+}
